a18af2bf761bb45d58e80551801d76f23c184735
Tanmay <gawandeth@gmail.com>
2026-05-10 19:47:13 +0530

    fix: required arguments in Transformer.infer method

--- model.py
+++ model.py
@@ -466,8 +466,8 @@ class Transformer(nn.Module):
 
     def __init__(
         self,
-        src_vocab_size: int,
-        tgt_vocab_size: int,
+        src_vocab_size: int = len(SRC_VOCAB),
+        tgt_vocab_size: int = len(TGT_VOCAB),
         d_model: int = 512,
         N: int = 6,
         num_heads: int = 8,
